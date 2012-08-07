98c9d49b50d0478f56414ced6d1954662f5716c8
kumarunster <nikolai.raitsev@gmail.com>
2012-08-07 16:42:01 +0200

    extended with view communication with osgi events

--- test.graphiti.nonemf/src/test/graphiti/nonemf/rcpapp/objectsrepository/ObjectsRepositoryViewNonEmf.java
+++ test.graphiti.nonemf/src/test/graphiti/nonemf/rcpapp/objectsrepository/ObjectsRepositoryViewNonEmf.java
@@ -1,28 +1,51 @@
 package test.graphiti.nonemf.rcpapp.objectsrepository;
 
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.util.LocalSelectionTransfer;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.part.ViewPart;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 
+import test.graphiti.nonemf.domainmodel.TermClass;
 import test.graphiti.nonemf.rcpapp.objectsrepository.dnd.DragListener;
+import test.graphiti.nonemf.utils.EventCommunicationConstants;
 import test.graphiti.nonemf.utils.RepositoryUtils;
 
 public class ObjectsRepositoryViewNonEmf extends ViewPart {
 
-	public static final String ID = "test.graphiti.nonemf.rcpapp.ObjectsRepositoryViewNonEmf"; //$NON-NLS-1$
+	public static final String ID = "test.graphiti.nonemf.rcpapp.objectsrepository.ObjectsRepositoryViewNonEmf"; //$NON-NLS-1$
+	private Action createNewAction;
 	private final FormToolkit toolkit = new FormToolkit(Display.getCurrent());
+	private TreeViewer treeViewer;
+	
 
 	public ObjectsRepositoryViewNonEmf() {
 	}
@@ -37,7 +60,7 @@ public class ObjectsRepositoryViewNonEmf extends ViewPart {
 		toolkit.paintBordersFor(container);
 		container.setLayout(new GridLayout(1, false));
 		{
-			TreeViewer treeViewer = new TreeViewer(container, SWT.BORDER | SWT.FULL_SELECTION);
+			treeViewer = new TreeViewer(container, SWT.BORDER | SWT.FULL_SELECTION);
 			Tree treeWithViewer = treeViewer.getTree();
 			TreeColumn trclmnNewColumn = new TreeColumn(treeWithViewer, SWT.NONE);
 			trclmnNewColumn.setWidth(80);
@@ -67,6 +90,7 @@ public class ObjectsRepositoryViewNonEmf extends ViewPart {
 		createActions();
 		initializeToolBar();
 		initializeMenu();
+		registerEventHandlers();
 	}
 
 	public void dispose() {
@@ -78,7 +102,27 @@ public class ObjectsRepositoryViewNonEmf extends ViewPart {
 	 * Create the actions.
 	 */
 	private void createActions() {
-		// Create the actions
+		createNewAction = new Action() {
+			@Override public void run() {
+				super.run();
+				System.out.println("Run Action!");
+				createNewAction_Body();
+			}
+		};
+		
+		
+		String text = "Create New Item in Repository";
+		createNewAction.setText(text);
+		createNewAction.setToolTipText(text);
+		
+		ImageDescriptor imgDescriptor = PlatformUI.getWorkbench()
+										.getSharedImages()
+										.getImageDescriptor(ISharedImages.IMG_OBJ_ADD);
+		
+		createNewAction.setImageDescriptor(imgDescriptor);
+		createNewAction.setEnabled(true);
+		
+		
 	}
 
 	/**
@@ -86,6 +130,9 @@ public class ObjectsRepositoryViewNonEmf extends ViewPart {
 	 */
 	private void initializeToolBar() {
 		IToolBarManager tbm = getViewSite().getActionBars().getToolBarManager();
+		
+		
+		tbm.add(createNewAction);
 	}
 
 	/**
@@ -93,11 +140,80 @@ public class ObjectsRepositoryViewNonEmf extends ViewPart {
 	 */
 	private void initializeMenu() {
 		IMenuManager manager = getViewSite().getActionBars().getMenuManager();
+		
+		manager.add(createNewAction);
+
+		MenuManager menuManager = new MenuManager();
+		Menu treeMenu = menuManager.createContextMenu(treeViewer.getTree());
+		// Set the MenuManager
+		treeViewer.getTree().setMenu(treeMenu);
+		getSite().registerContextMenu(menuManager, treeViewer);
+		// Make the selection available
+		getSite().setSelectionProvider(treeViewer);
+		
+		MenuItem menuItemCreateNew = new MenuItem(treeMenu, SWT.NONE);
+		menuItemCreateNew.setImage(createNewAction.getImageDescriptor().createImage());
+		menuItemCreateNew.setText(createNewAction.getText());
+		menuItemCreateNew.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				super.widgetSelected(e);
+				System.out.println("selected");
+				
+				createNewAction.run();
+			}			
+		});
 	}
 
 	@Override
 	public void setFocus() {
 		// Set the focus
 	}
+	
+
+	private void registerEventHandlers() {
+		BundleContext bundleContext = FrameworkUtil.getBundle(ObjectsRepositoryViewNonEmf.class).getBundleContext();
+		
+		EventHandler eventHandler = new EventHandler() {
+			@Override
+			public void handleEvent(Event event) {	
+				System.out.println("handle osgi service event!");
+				
+				Display.getDefault().asyncExec(new Runnable() {
+					@Override
+					public void run() {
+						updateTreeView();
+					}
+				});
+			}		
+		};
+		
+		
+		Dictionary<String,String> properties = new Hashtable<String, String>();
+		properties.put(EventConstants.EVENT_TOPIC, EventCommunicationConstants.VIEWCOMMUNICATION + "/*");
+		bundleContext.registerService(EventHandler.class, eventHandler, properties);
+		
+	}
+	
+	
+
+	protected void createNewAction_Body() {
+		
+		TermClass newTermClass = new TermClass();
+		newTermClass.setName("give me a name!");
+		//as a side effect puts the bo into the objects map...
+		RepositoryUtils.pojoIndependenceSolverStatic.getKeyForBusinessObject(newTermClass);
+		
+		updateTreeView();
+	}
+
+
+	private void updateTreeView() {
+		
+		treeViewer.setInput(RepositoryUtils.pojoIndependenceSolverStatic);
+		
+		treeViewer.expandToLevel(1);
+	}	
 
 }
