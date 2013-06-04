9cbac95f610a9aba0fb780fd4e7225343ab8a938
kumarunster <nikolai.raitsev@gmail.com>
2013-06-04 17:24:41 +0200

    added client datamodel js

---
+++ vertx-yoke-groovy-example/src/main/resources/static/js/datamodel/model.js
@@ -0,0 +1,71 @@
+var GenderType = stjs.enumeration2( 
+	{
+		"MALE": {code: "M", translationCode: "male"}, 
+		"FEMALE": {code: "F", translationCode: "female"}
+	}
+	,
+	{
+		"getCode" : function() {
+			return this.code;
+		},
+		
+		"getTranslationCode" : function() {
+			return this.translationCode;
+		}
+		
+	},
+	{
+		"findByCode" : function(code) {
+			for(i in GenderType.values()) {
+				console.log("in findByCode, i is: " + i);
+				if(GenderType.values()[i].code === code)
+					return GenderType.values()[i];
+			}
+		}
+	}
+);
+
+/*
+var GenderType = stjs.enumeration(
+    "MALE", 
+    "FEMALE"
+);
+*/
+
+var User = function(){};
+stjs.extend(User, null, [], function(constructor, prototype){
+    prototype.fName = null;
+    prototype.lName = null;
+    prototype.email = null;
+    prototype.gender = null;
+    prototype.getfName = function() {
+        return this.fName;
+    };
+    prototype.setfName = function(fName) {
+        this.fName = fName;
+    };
+    prototype.getlName = function() {
+        return this.lName;
+    };
+    prototype.setlName = function(lName) {
+        this.lName = lName;
+    };
+    prototype.getEmail = function() {
+        return this.email;
+    };
+    prototype.setEmail = function(email) {
+        this.email = email;
+    };
+    prototype.getGender = function() {
+        return this.gender;
+    };
+    prototype.setGender = function(gender) {
+        this.gender = gender;
+    };
+	constructor.createFromJSON = function(json) {
+        return stjs.parseJSON(json, User);
+    };
+}, {"gender":{name:"Enum", arguments:["GenderType"]}});
+
+
+
