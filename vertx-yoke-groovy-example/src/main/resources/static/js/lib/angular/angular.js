6aa354083b5a96b0310eed236d9ffdc7b4f38dee
kumarunster <nikolai.raitsev@gmail.com>
2013-07-16 01:08:55 +0200

    fixes angular.js problem with no existent keydown/keypress bindings, see http://aerilys.fr/blog/?p=1218

--- vertx-yoke-groovy-example/src/main/resources/static/js/lib/angular/angular.js
+++ vertx-yoke-groovy-example/src/main/resources/static/js/lib/angular/angular.js
@@ -12976,7 +12976,7 @@ var ngCspDirective = ['$sniffer', function($sniffer) {
  */
 var ngEventDirectives = {};
 forEach(
-  'click dblclick mousedown mouseup mouseover mouseout mousemove mouseenter mouseleave'.split(' '),
+  'click dblclick mousedown mouseup mouseover mouseout mousemove mouseenter mouseleave keydown keyup touchstart touchend'.split(' '),
   function(name) {
     var directiveName = directiveNormalize('ng-' + name);
     ngEventDirectives[directiveName] = ['$parse', function($parse) {
